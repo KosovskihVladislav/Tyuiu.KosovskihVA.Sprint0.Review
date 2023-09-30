b39c9853e7bb8db354275aafc9d742c2c47b818a
Tyuiu.kosovskihVA@outlook.com <mr.kosovskih35@mail.ru>
2023-09-30 17:46:30 +0500

    Add project files.

---
+++ Tyuiu.KosovskihVA.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KosovskihVA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KosovskihVA.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            Assert.AreEqual(35, DataService.Calc(2, 2, 3));
+        }
+    }
+}
---
+++ Tyuiu.KosovskihVA.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosovskihVA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KosovskihVA.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Косовских В. А. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0 Review                                                        *");
+            Console.WriteLine("*Тема: Создания итогового решения по спринту                              *");
+            Console.WriteLine("*Задание #Review                                                          *");
+            Console.WriteLine("*Вариант #0                                                               *");
+            Console.WriteLine("*Выполнил: Косовских Владислав Алексеевич | ПКТб 23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значение трех    *");
+            Console.WriteLine("* чисел и умножает их на 5.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            // Место решения задания
+            int x = 2, y = 2, z = 4;
+            Console.WriteLine(DataService.Calc(x, y, z));
+        }
+    }
+}
